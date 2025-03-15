69cd92feb02d18d775b7d0a45c826fa3c5858a40
Balychev Kiril <mister.balychev@gmail.com>
2025-03-15 15:09:36 +0200

    first commit

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	// TODO : INIT CONF clean env
+
+	// TODO : LOGGER slog
+
+	//TODO : STORAGE sqlite3
+
+	//TODO : INIT ROUTER chi, render
+
+	//TODO : RUN SERVER
+
+}
