64644c175e097d5e92a67f3fc28150d0ea62f4b8
Balychev Kiril <mister.balychev@gmail.com>
2025-03-15 16:42:24 +0200

    Implemented logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,8 +1,24 @@
 package main
 
-func main() {
-	// TODO : INIT CONF clean env
+import (
+	"log/slog"
+	"os"
+	"url-shortener/internal/config"
+)
 
+const (
+	envLocal = "local"
+	envProd  = "prod"
+	envDev   = "dev"
+)
+
+func main() {
+
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("Starting url-shortener")
 	// TODO : LOGGER slog
 
 	//TODO : STORAGE sqlite3
@@ -11,4 +27,25 @@ func main() {
 
 	//TODO : RUN SERVER
 
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
